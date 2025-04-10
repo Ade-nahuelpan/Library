47647b92b872f859e02fb4d20ccf5c79db4f69ee
Ade Nahuelpan <ade.nahuelpan.s@gmail.com>
2025-04-10 17:48:54 -0400

    DB connection change

--- db/connection.js
+++ db/connection.js
@@ -1,26 +1,7 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+import mongoose from 'mongoose';
 
-const uri = process.env.ATLAS_URI || "";
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
-try {
-  // Connect the client to the server
-  await client.connect();
-  // Send a ping to confirm a successful connection
-  await client.db("admin").command({ ping: 1 });
-  console.log(
-   "Pinged your deployment. You successfully connected to MongoDB!"
-  );
-} catch(err) {
-  console.error(err);
+export default function initDb() {
+  mongoose.connect(process.env.DB_CONNECT_URL, {
+    dbName: process.env.DB_NAME,
+  });
 }
-
-let db = client.db("employees");
-
-export default db;
